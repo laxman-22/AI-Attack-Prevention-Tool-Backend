d1e33354c735f720a24033884cfddb7a9a379879
Aryan <aryanlaxmansingh22@gmail.com>
2024-12-05 05:32:07 -0500

    optimized model

--- model.py
+++ model.py
@@ -1,49 +1,24 @@
+import onnxruntime
+import numpy as np
 import torch
-from torchvision import models
-import torch.nn as nn
 
-class FineTunedResNet50(nn.Module):
-    def __init__(self):
-        super(FineTunedResNet50, self).__init__()
-        self.resnet = models.resnet50(weights='DEFAULT')
+# Load the ONNX model (optimized model)
+onnx_model_path = 'fine_tuned_resnet50_optimized.onnx'
+session = onnxruntime.InferenceSession(onnx_model_path)
 
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-        
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 1)
-        
-        self.attack_output = nn.Linear(self.resnet.fc.in_features, 5)
-
-    def forward(self, x):
-        x = self.resnet.conv1(x)
-        x = self.resnet.bn1(x)
-        x = self.resnet.relu(x)
-        x = self.resnet.maxpool(x)
-        x = self.resnet.layer1(x)
-        x = self.resnet.layer2(x)
-        x = self.resnet.layer3(x)
-        x = self.resnet.layer4(x)
-        x = self.resnet.avgpool(x)
-        
-        x = torch.flatten(x, 1)
-        
-        binary_pred = torch.sigmoid(self.resnet.fc(x)).squeeze()
-        
-        attack_pred = self.attack_output(x)
-        
-        return binary_pred, attack_pred
-    
 def predict(img):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = FineTunedResNet50().to(device)
-    model.load_state_dict(torch.load('imp_binary_model.pth', map_location=device))
-    model.eval()
+    # Preprocess image
+    img = img.numpy() if torch.is_tensor(img) else np.array(img)
+    img = np.expand_dims(img, axis=0)
 
-    with torch.no_grad():
-        binary_pred, attack_pred = model(img)
+    img = img.astype(np.float32)
 
-    binary_pred = binary_pred.item()
-    attack_pred = attack_pred.argmax(dim=1).item()
+    # Run the model
+    inputs = {session.get_inputs()[0].name: img}
+    outputs = session.run(None, inputs)
+
+    binary_pred = outputs[0].squeeze()
+
+    attack_pred = np.argmax(outputs[1], axis=1).item()
 
     return binary_pred, attack_pred
-
