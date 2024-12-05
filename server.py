1a613b24865939bb45bc8feeae9ab0372f677595
Aryan <aryanlaxmansingh22@gmail.com>
2024-12-05 06:24:18 -0500

    squeeze

--- server.py
+++ server.py
@@ -276,9 +276,8 @@ def generatePrediction():
             res = jsonify({"error": "No image has been provided or set for prediction"})
             response = make_response(res)
             return response, 400
-        print(img_to_predict.shape)
-        if len(img_to_predict.shape) == 3:
-            img_to_predict = img_to_predict.unsqueeze(0)
+        
+        img_to_predict = img_to_predict.squeeze()
 
         # Create a user friendly mapping
         binary_pred, attack_pred = predict(img_to_predict)
